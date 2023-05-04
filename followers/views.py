10a2c6287c1acc203a2391dc4b1b36dfcb62df05
Undie92 <Undie92@hotmail.com>
2023-05-04 09:28:17 +0000

    Added follower section to database

---
+++ followers/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, permissions
+from backup.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+    
