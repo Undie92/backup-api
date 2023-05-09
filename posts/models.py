ddb319993c43019a89b4aac1d6421820b3ed7ca6
Undie92 <Undie92@hotmail.com>
2023-05-09 11:45:39 +0000

    Added category to post model

--- posts/models.py
+++ posts/models.py
@@ -23,6 +23,7 @@ class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/', default='../default_post_mzgj1g', blank=True)
